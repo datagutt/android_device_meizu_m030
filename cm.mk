c900bb2906bac4edbc45dd86575b010d1df59a68
datagutt <datagutt666@hotmail.com>
2012-10-26 22:00:12 +0200

    oops

--- cm.mk
+++ cm.mk
@@ -2,17 +2,17 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := m9
+PRODUCT_RELEASE_NAME := mx
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/meizu/m9/device_m9.mk)
+$(call inherit-product, device/meizu/mx/device_mx.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := m9
-PRODUCT_NAME := cm_meizu_m9
+PRODUCT_DEVICE := mx
+PRODUCT_NAME := cm_meizu_mx
 PRODUCT_BRAND := Meizu
-PRODUCT_MODEL := M9
+PRODUCT_MODEL := MX
 PRODUCT_MANUFACTURER := Meizu
