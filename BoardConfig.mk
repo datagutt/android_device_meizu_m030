255f05fa3b84dcf205a751062ccb09eec5662f7a
datagutt <datagutt666@hotmail.com>
2013-03-05 20:49:19 +0100

    new stuff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+-include device/samsung/smdk4412-common/BoardCommonConfig.mk
 
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
